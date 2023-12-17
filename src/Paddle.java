16903f94a9171885e27137d63e93029ae8ea8796
R Sarveshvarn <151063233+gdev-kingx@users.noreply.github.com>
2023-12-17 15:16:36 +0530

    Add files via upload

---
+++ src/Paddle.java
@@ -0,0 +1,72 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class Paddle extends Rectangle{
+
+    int id;
+    int yVel;
+    int speed = 10;
+
+    Paddle(int x, int y, int PADDLE_W, int PADDLE_H, int id) {
+        super(x, y, PADDLE_W, PADDLE_H);
+        this.id = id;
+    }
+
+    public void keyPressed(KeyEvent e) {
+		switch(id) {
+		case 1:
+			if(e.getKeyCode()==KeyEvent.VK_W) {
+				setYDirection(-speed);
+			}
+			if(e.getKeyCode()==KeyEvent.VK_S) {
+				setYDirection(speed);
+			}
+			break;
+		case 2:
+			if(e.getKeyCode()==KeyEvent.VK_UP) {
+				setYDirection(-speed);
+			}
+			if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+				setYDirection(speed);
+			}
+			break;
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+		switch(id) {
+		case 1:
+			if(e.getKeyCode()==KeyEvent.VK_W) {
+				setYDirection(0);
+			}
+			if(e.getKeyCode()==KeyEvent.VK_S) {
+				setYDirection(0);
+			}
+			break;
+		case 2:
+			if(e.getKeyCode()==KeyEvent.VK_UP) {
+				setYDirection(0);
+			}
+			if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+				setYDirection(0);
+			}
+			break;
+		}
+	}
+
+	public void setYDirection(int yDirection) {
+		yVel = yDirection;
+	}
+
+	public void move() {
+		y= y + yVel;
+	}
+
+	public void draw(Graphics g) {
+		if(id==1)
+			g.setColor(Color.blue);
+		else
+			g.setColor(Color.red);
+		g.fillRect(x, y, width, height);
+	}
+}
